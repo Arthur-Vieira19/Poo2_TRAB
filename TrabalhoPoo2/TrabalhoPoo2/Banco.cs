5e98e4c94f48aee37cb654c240712313271502e4
Arthur-Vieira19 <160856620+Arthur-Vieira19@users.noreply.github.com>
2025-11-11 08:35:52 -0300

    Add files via upload

---
+++ TrabalhoPoo2/TrabalhoPoo2/Banco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoPoo2
+{
+    internal class Banco
+    {
+        public string Nome { get; set; }
+        public string Codigo { get; set; }
+
+        public GerenciadoDeContas gerContas { get; set; }
+        public GerenciadorDeClientes gerClientes { get; set; }
+        public GerenciadorDeTransacoes gerTransacoes { get; set; }
+        public Calendario Calendario { get; set; }
+
+        
+        public void adicionarCliente(Cliente cliente) { }
+        public void removerCliente(Cliente cliente) { }
+    }
+}
---
+++ TrabalhoPoo2/TrabalhoPoo2/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoPoo2
+{
+    internal class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public List<Conta> Contas { get; set; }
+        
+        public void AdicionarConta(Conta conta)
+        {
+            foreach (var c in Contas)
+            {
+                if(c.Numero == conta.Numero)
+                {
+                    Console.WriteLine("Conta já existe para este cliente.");
+                    return;
+                }
+                if(c.)
+            }
+        }
+        public void RemoverConta(Conta conta) { }
+        public void ListarContas() { }
+    }
+}

87d7c444dfc2b49cae4fe3d6972275fd895a90f0
Arthur-Vieira19 <160856620+Arthur-Vieira19@users.noreply.github.com>
2025-11-11 08:48:14 -0300

    GITCOMMIT

---
+++ TrabPoo2/Calendario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabPoo2
+{
+    class Calendario
+    {
+        public DateTime DataAtual { get; set; }
+
+        public void AvancarDia() { }
+        public void RetrocederDia() { }
+        public void ExibirDataAtual() { }
+    }
+}
---
+++ TrabPoo2/Conta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabPoo2
+{
+    class Conta
+    {
+        public string Numero { get; set; }
+        public string Tipo { get; set; }
+        public decimal Saldo { get; set; }
+        public Cliente Titular { get; set; }
+        public void Depositar(decimal valor) { }
+        public void Sacar(decimal valor) { }
+        public void Transferir(Conta destino, decimal valor) { }
+    }
+}

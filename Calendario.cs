ef0fa205a3441fa871cb44aca0a277ae70139a39
ThiagoGama1 <160535848+ThiagoGama1@users.noreply.github.com>
2025-11-14 08:44:25 -0300

    Add files via upload

---
+++ Calendario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabPoo2
+{
+    class Calendario
+    {
+        public DateTime DataAtual { get; set; }
+
+        public void AvancarDia() {
+            DataAtual.AddDays(1);
+        }
+        public void RetrocederDia()
+        {
+            DataAtual.AddDays(-1);
+        }
+        public void ExibirDataAtual()
+        {
+            Console.WriteLine("Data:\n" + DataAtual);
+        }
+    }
+}
---
+++ Sacar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabPoo2
+{
+    class Sacar : Transacao
+    {
+        public bool Executar()
+        {
+            return true;
+        }
+        public bool executarSaque(Cliente cliente, Conta conta, decimal valor)
+        {
+            if(cliente == null)
+            {
+                Console.WriteLine("Cliente inválido! ");
+                return false;
+            }
+            if (cliente.Contas.Contains(conta) && conta.Saldo >= valor)
+            {
+                conta.Saldo -= valor;
+                return Executar();
+            }
+            return false;
+        }
+    }
+}

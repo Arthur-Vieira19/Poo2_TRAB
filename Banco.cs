db81893539e09698a2ef1241e6387fc5c88fe7d7
Arthur-Vieira19 <160856620+Arthur-Vieira19@users.noreply.github.com>
2025-11-14 08:42:37 -0300

    Add files via upload
    TMJ EH NOIS

---
+++ Banco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabPoo2
+{
+        class Banco
+        {
+                public string Nome { get; set; }
+                public string Codigo { get; set; }
+
+                public GerenciadorDeContas gerContas { get; set; }
+                public GerenciadorDeClientes gerClientes { get; set; }
+                public GerenciadorDeTransacoes gerTransacoes { get; set; }
+                public Calendario Calendario { get; set; }
+
+
+                public void adicionarCliente(Cliente cliente) { }
+                public void removerCliente(Cliente cliente) { }
+        }
+    }
---
+++ GerenciadorDeClientes.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabPoo2
+{
+    class GerenciadorDeClientes
+    {
+        private List<Cliente> clientes;
+        public GerenciadorDeClientes() {
+            clientes = new List<Cliente>();
+        }
+        public void Adicionar(Cliente cliente) {
+            foreach (Cliente c in clientes) {
+                if (c.Id == cliente.Id) {
+                    Console.WriteLine("CLIENTE JA CADASTRADO!");
+                
+                }
+            
+            }
+            clientes.Add(cliente);
+
+        }
+
+        public void Remover(Cliente cliente) {
+
+            clientes.RemoveAll(c => c.Id == cliente.Id);
+
+        }
+
+        public Cliente BuscarPorCPF(string cpf) {
+            foreach (Cliente c1 in clientes) {
+                if (c1.CPF == cpf)
+                {
+
+                    Console.WriteLine($"O CPF: {cpf}, foi encontrado!");
+                    
+                }
+
+            }
+
+
+
+
+            Console.WriteLine($"CPF {cpf}, nao encontrado");
+
+            return null;
+        }
+        public Cliente BuscarPorId(int id) {
+            foreach (Cliente c in clientes)
+            { if (c.Id == id)
+                {
+                    Console.WriteLine("CLIENTE ENCONTRADO!");
+
+                }
+            }
+
+            Console.WriteLine("CLIENTE INEXISTENTE!");
+            return null;
+
+
+        }
+
+        public List<Cliente> ListarClientes() {
+
+            foreach (Cliente c in clientes) {
+                Console.WriteLine(c);
+            }
+            return null;
+        }
+    } 
+}

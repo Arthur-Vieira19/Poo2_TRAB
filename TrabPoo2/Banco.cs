31ff06de11a9d202e3939669259b659a5d463426
Arthur-Vieira19 <160856620+Arthur-Vieira19@users.noreply.github.com>
2025-11-14 08:48:27 -0300

    Add files via upload
    AGORA SIM TMJ EH TOIS

--- TrabPoo2/Banco.cs
+++ TrabPoo2/Banco.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace TrabPoo2
 {
-    class Banco
-    {
-        public string Nome { get; set; }
-        public string Codigo { get; set; }
+        class Banco
+        {
+                public string Nome { get; set; }
+                public string Codigo { get; set; }
 
-        public GerenciadorDeContas gerContas { get; set; }
-        public GerenciadorDeClientes gerClientes { get; set; }
-        public GerenciadorDeTransacoes gerTransacoes { get; set; }
-        public Calendario Calendario { get; set; }
+                public GerenciadorDeContas gerContas { get; set; }
+                public GerenciadorDeClientes gerClientes { get; set; }
+                public GerenciadorDeTransacoes gerTransacoes { get; set; }
+                public Calendario Calendario { get; set; }
 
 
-        public void adicionarCliente(Cliente cliente) { }
-        public void removerCliente(Cliente cliente) { }
+                public void adicionarCliente(Cliente cliente) { }
+                public void removerCliente(Cliente cliente) { }
+        }
     }
-}

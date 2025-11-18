9d8f83ee12d5e2577f3a91c81c10436bf7245465
ThiagoGama1 <160535848+ThiagoGama1@users.noreply.github.com>
2025-11-17 22:03:45 -0300

    Add files via upload

--- TrabPoo2/Cliente.cs
+++ TrabPoo2/Cliente.cs
@@ -14,19 +14,58 @@ namespace TrabPoo2
         public string Endereco { get; set; }
         public List<Conta> Contas { get; set; }
 
-        public void AdicionarConta(Conta conta)
+
+        public Cliente(int id, string nome, string cpf, string endereco)
         {
-            foreach (var c in Contas)
-            {
-                if (c.Numero == conta.Numero)
+            Id = id;
+            Nome = nome;
+            CPF = cpf;
+            Endereco = endereco;
+            Contas = new List<Conta>();
+        }
+
+        public bool AdicionarConta(Conta conta)
+        {
+            
+                if (conta == null)
                 {
-                    Console.WriteLine("Conta já existe para este cliente.");
-                    return;
+                    throw new ArgumentNullException(nameof(conta), "Conta não pode ser nula.");
+
                 }
-                if (c.)
+                
+                bool existe = Contas.Any(c => c.Numero == conta.Numero);
+                
+                if (existe)
+                {
+                    return false;
+                }
+
+            this.Contas.Add(conta);
+                return true;
+            
+        }
+        public bool RemoverConta(Conta conta) {
+            if(conta == null)
+            {
+                throw new ArgumentNullException(nameof(conta), "Conta não pode ser nula.");
+                
+            }
+            var contaEncontrada = Contas.FirstOrDefault(c => c.Numero == conta.Numero);
+
+            if (contaEncontrada == null)
+                return false;
+
+            this.Contas.Remove(contaEncontrada);
+            return true;
+
+
+        }
+        public void ListarContas()
+        {
+            foreach (var conta in Contas)
+            {
+                Console.WriteLine(conta);
             }
         }
-        public void RemoverConta(Conta conta) { }
-        public void ListarContas() { }
     }
 }
